bbe15a719e546dd2e019350c84d76da530c4cf06
Cooky <escobamatos@gmail.com>
2025-09-09 03:20:40 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const JUGNU = require("./handlers/Client");
+const MUSICBOT = require("./handlers/Client");
 const { TOKEN } = require("./settings/config");
 
-const client = new JUGNU();
+const client = new MUSICBOT();
 
 module.exports = client;
 
